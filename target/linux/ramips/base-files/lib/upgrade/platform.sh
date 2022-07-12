8f69787d822a227698b38566361c65a1b289841e
openwrtdiy <57249132+openwrtdiy@users.noreply.github.com>
2022-07-12 19:42:59 +0800

    Revert "ramips: add xiaomi/redmi ac2100 support"
    This reverts commit 9d85b7dee8b8e7fdfe85555e5df74486cb6b5ea7.

--- target/linux/ramips/base-files/lib/upgrade/platform.sh
+++ target/linux/ramips/base-files/lib/upgrade/platform.sh
@@ -60,9 +60,8 @@ platform_do_upgrade() {
 	netgear,r6350|\
 	ubnt-erx|\
 	ubnt-erx-sfp|\
-	xiaomi,mir3p|\
-	xiaomi,mi-ac2100|\
-	xiaomi,redmi-ac2100)
+	xiaomi,mir3g|\
+	xiaomi,mir3p)
 		nand_do_upgrade "$1"
 		;;
 	tplink,c50-v4)
